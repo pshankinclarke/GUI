dc1b0219ff0b93b8f5d47f285c8c37cb14d383f1
pshankinclarke <38367844+pshankinclarke@users.noreply.github.com>
2018-05-24 22:37:42 -0700

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -12,33 +12,10 @@ from PIL import ImageTk, Image
 
 class GUI : 
     
-    def __init__(self,path):
-        self.path = path
-    
-    def createAndShowGUI(self,path) :
-        
-        #This creates the main window of an application
-        window = tk.Toplevel()
-        window.title("Join")
-        window.geometry("300x300")
-        window.configure(background='grey')
-
-        
-        #Creates a Tkinter-compatible photo image, which can be used everywhere Tkinter expects an image object.
-        img = ImageTk.PhotoImage(Image.open(path))
-        
-        #The Label widget is a standard Tkinter widget used to display a text or image on the screen.
-        panel = tk.Label(window, image = img)
-        
-        #The Pack geometry manager packs widgets in rows or columns.
-        panel.pack(side = "bottom", fill = "both", expand = "yes")
-        
-        #Start the GUI
-        window.mainloop()
-        
-        place = GUI('test.png')
-        
-#        path = 'test.png'
+#    def __init__(self,path):
+#        self.path = path
+#    
+#    def createAndShowGUI(self,path) :
 #        
 #        #This creates the main window of an application
 #        window = tk.Toplevel()
@@ -58,4 +35,28 @@ class GUI :
 #        
 #        #Start the GUI
 #        window.mainloop()
+#        
+#        place = GUI('test.png')
+#        
+
+        path = 'test.png'
+        
+        #This creates the main window of an application
+        window = tk.Toplevel()
+        window.title("Join")
+        window.geometry("300x300")
+        window.configure(background='grey')
+
+        
+        #Creates a Tkinter-compatible photo image, which can be used everywhere Tkinter expects an image object.
+        img = ImageTk.PhotoImage(Image.open(path))
+        
+        #The Label widget is a standard Tkinter widget used to display a text or image on the screen.
+        panel = tk.Label(window, image = img)
+        
+        #The Pack geometry manager packs widgets in rows or columns.
+        panel.pack(side = "bottom", fill = "both", expand = "yes")
+        
+        #Start the GUI
+        window.mainloop()
         
